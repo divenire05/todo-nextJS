9311264feb1f216349f707e0a632f19eacd62765
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-23 03:24:32 -0500

    header and footer

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='fixed top-0 w-full left-0 bg-green-300'>Header</div>
+  )
+}
--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
 
 export default function Layout(props) {
     const {children} = props
   return (
     <div className='flex flex-col min-h-screen relative bg-slate-900'>
-        {children}
+        <Header/>
+        <main className='flex-1'>
+            {children}
+        </main>
+        <Footer/>
     </div>
   )
 }
