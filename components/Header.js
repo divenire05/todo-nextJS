02fb688abb02b0498b266a76c6ac4b5fed72fce0
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-23 12:41:46 -0500

    creating header

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div className='fixed top-0 w-full left-0 bg-green-300'>Header</div>
+    <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 px-6 border-b border-solid border-white'>
+        <h1 className='text-3xl select-none sm:text-6xl'>TODO LIST</h1>
+        <i className="fa-solid fa-user text-xl sm:text-3xl duration-300 hover:opacity-40 cursor-pointer"></i>
+    </div>
   )
 }
