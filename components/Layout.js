c141058bd0f23d91301306374b5cc10408158344
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-23 03:18:13 -0500

    layout

---
+++ components/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Layout(props) {
+    const {children} = props
+  return (
+    <div className='flex flex-col min-h-screen relative bg-slate-900'>
+        {children}
+    </div>
+  )
+}
